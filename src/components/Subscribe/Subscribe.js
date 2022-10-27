5d2605c2391010e7d4587c254a92b2614888c857
Yalle Vieira <yallevieira.vos@gmail.com>
2022-10-27 20:44:26 -0300

    adicionando onSubmit em subscribe

--- src/components/Subscribe/Subscribe.js
+++ src/components/Subscribe/Subscribe.js
@@ -6,12 +6,21 @@ import Email from "../../assets/img/social-media/email.png";
 import Facebook from "../../assets/img/social-media/facebook.png";
 
 const Subscribe = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert("We are working on it.");
+  };
+
   return (
     <Container>
       <h3>Don't miss the update</h3>
       <form className="form-subscribe">
         <input placeholder="Enter your email" />
-        <button className="btn btn-subscribe" type="submit">
+        <button
+          onClick={handleSubmit}
+          className="btn btn-subscribe"
+          type="submit"
+        >
           Subscribe
         </button>
       </form>
